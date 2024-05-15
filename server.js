c35b0efe19b69e496251ede013e59379bd882049
agustinneuman <agustin.neuuman@snagajob.com>
2024-05-15 14:01:38 -0300

    cambio rutas base

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 import express from 'express';
 import cartRouter from './route/cart.router.js'
 import productsRouter from './route/product.router.js';
-// import morgan from 'morgan';
-// import { __dirname } from './path.js';
-// import { errorHandler } from './middlewares/errorHandler.js';
 
 const app = express();
-
-// app.use(express.static(__dirname + '/public'))
-// app.use(express.urlencoded({extended:true}))
-// app.use(morgan('dev'))
 app.use(express.json())
 
-app.use('/cart', cartRouter);
-app.use('/product', productsRouter);
-
-//app.use(errorHandler);
+app.use('/api/cart', cartRouter);
+app.use('/api/product', productsRouter);
 
 app.listen(8080,()=>{
     console.log(`Escuchando en el puerto 8080`)
