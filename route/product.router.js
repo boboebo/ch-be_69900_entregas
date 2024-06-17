1c838e7f41bf068122ce1775e7ee4e35dc4e4cea
agustinneuman <agustin.neuuman@snagajob.com>
2024-06-17 08:03:30 -0300

    Revert "soporte para websocket"
    This reverts commit b5e81b1f8cc5a3665d38a45da3e4b1f15d2f5d95.

--- route/product.router.js
+++ route/product.router.js
@@ -2,57 +2,55 @@ import { Router } from "express";
 import ProductManager from "../manager/productManager.js";
 import { validateProd } from "../middleware/validateProd.js";
 
-const createProductsRouter = (io) => {
-  const router = Router();
-  const path = "data/products.json";
-  const pm = new ProductManager(path);
+const router = Router();
+const path = "data/products.json";
 
-  // get de productos
-  router.get("/", async (req, res) => {
-    const { limit } = req.query;
-    const products = await pm.getProducts(limit);
-    res.status(200).json(products);
-  });
+const pm = new ProductManager(path);
 
-  // get producto by id
-  router.get("/:pid", async (req, res) => {
-    const { pid } = req.params;
-    const product = await pm.getProductById(pid);
-    res.status(200).json(product);
-  });
+// get de productos
+router.get("/", async (req, res) => {
+  const { limit } = req.query;
+  const products = await pm.getProducts(limit);
+  res.status(200).json(products);
+});
 
-  // post add producto
-  router.post("/", validateProd, async (req, res) => {
-    const product = req.body;
-    console.log("product en post -> ", product);
-    const productCreated = pm.createProduct(product);
-    io.emit('productCreated', productCreated);
-    res.json(productCreated);
-  });
+// get producto by id
+router.get("/:pid", async (req, res) => {
+  const { pid } = req.params;
+  const product = await pm.getProductById(pid);
+  res.status(200).json(product);
+});
 
-  // put update product by id
-  router.put("/:pid", async (req, res) => {
-    const { pid } = req.params;
-    const prodUpd = await pm.updateProduct(pid, req.body);
-    if (!prodUpd) res.status(404).json({ msg: "Error updating prod" });
-    res.status(200).json(prodUpd);
-  });
+// post add producto
+/* 
+  id: uuid
+  title str
+  desc str
+  code str
+  price number
+  status: true
+  stock number
+  thumbnails: optional
+*/
+router.post("/", validateProd, async (req, res) => {
+  const product = req.body;
+  const productCreated = pm.createProduct(product);
+  res.json(productCreated);
+});
 
-  //delete product by id
-  router.delete("/:pid", async (req, res) => {
-    const { pid } = req.params;
-    const product = await pm.deleteProductById(pid);
-    io.emit('productDeleted', pid);
-    res.status(200).json(product);
-  });
+// put update product by id
+router.put("/:pid", async (req, res) => {
+  const { pid } = req.params;
+  const prodUpd = await pm.updateProduct(pid, req.body);
+  if (!prodUpd) res.status(404).json({ msg: "Error updating prod" });
+  res.status(200).json(prodUpd);
+});
 
-  // ruta para mostrar en vista la lista de productos
-  router.get("/listaRTProductos", (req, res) => {
-    console.log("tomaaaaa");
-    res.render("home");
-  });
+//delete product by id
+router.delete("/:pid", async (req, res) => {
+  const { pid } = req.params;
+  const product = await pm.deleteProductById(pid);
+  res.status(200).json(product);
+});
 
-  return router;
-};
-
-export default createProductsRouter;
+export default router;
--- server.js
+++ server.js
@@ -1,92 +1,13 @@
-import express from "express";
-import cartRouter from "./route/cart.router.js";
-import createProductsRouter from "./route/product.router.js";
-import handlebars from "express-handlebars";
-import { __dirname } from "./utils.js";
-import { Server } from "socket.io";
+import express from 'express';
+import cartRouter from './route/cart.router.js'
+import productsRouter from './route/product.router.js';
 
 const app = express();
-app.use(express.json());
-app.use(express.static(__dirname + "/public"));
+app.use(express.json())
 
-app.engine("handlebars", handlebars.engine());
-app.set("view engine", "handlebars");
-app.set("views", __dirname + "/views");
+app.use('/api/cart', cartRouter);
+app.use('/api/product', productsRouter);
 
-app.use(
-  express.static(__dirname + "/public", {
-    setHeaders: (res, path, stat) => {
-      if (path.endsWith(".css")) {
-        res.setHeader("Content-Type", "text/css");
-      }
-    },
-  })
-);
-
-app.get("/realTimeProducts", (req, res) => {
-  res.render("realTimeProducts");
-});
-
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-const httpServer = app.listen(8080, () => {
-  console.log(`Escuchando en el puerto 8080`);
-});
-
-const socketServer = new Server(httpServer);
-
-socketServer.on("connection", (socket) => {    
-  console.log(`user conectado ${socket.id}`);
-  socket.emit("loadProducts", "Cargando productos...");
-
-  socket.on("newProducto", async (prod) => {
-    try {
-      const response = await fetch('http://localhost:8080/api/product', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(prod)
-      });
-
-      if (!response.ok) {
-        throw new Error('Network response was not ok ' + response.statusText);
-      }
-
-      const productCreated = await response.json();
-      console.log('Producto creado:', productCreated);
-
-      io.emit('productCreated', productCreated);
-
-    } catch (error) {
-      console.error('Error al crear el producto:', error);
-    }
-  });
-
-  socket.on("deleteProducto", async (id) => {
-    console.log(`El elemento ${id} será eliminado`);
-    try {
-      const response = await fetch(`http://localhost:8080/api/product/${id}`, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-
-      if (!response.ok) {
-        throw new Error('Network response was not ok ' + response.statusText);
-      }
-      const productDeleted = await response.json();
-      console.log('Producto eliminado:', productDeleted);
-      io.emit('productDeleted', id);
-
-    } catch (error) {
-      console.error('Error al borrar el producto:', error);
-    }
-  });
-});
-
-app.use("/api/cart", cartRouter);
-app.use("/api/product", createProductsRouter(socketServer));
+app.listen(8080,()=>{
+    console.log(`Escuchando en el puerto 8080`)
+})
