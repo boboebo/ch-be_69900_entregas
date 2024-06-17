c85a13286597f69a446b8a2e33d25e0b362995ef
agustinneuman <agustin.neuuman@snagajob.com>
2024-06-17 08:02:55 -0300

    Revert "correcciones preentrega 1"
    This reverts commit 3050b9702e8b9a56750ee31a7710806d7d7c10df.

--- route/product.router.js
+++ route/product.router.js
@@ -24,7 +24,8 @@ const createProductsRouter = (io) => {
   // post add producto
   router.post("/", validateProd, async (req, res) => {
     const product = req.body;
-    const productCreated = await pm.createProduct(product);
+    console.log("product en post -> ", product);
+    const productCreated = pm.createProduct(product);
     io.emit('productCreated', productCreated);
     res.json(productCreated);
   });
