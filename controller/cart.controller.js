617066083e3450a5c2ce299db1b3b78f4bbe9e35
agustinneuman <agustin.neuuman@snagajob.com>
2024-06-20 22:58:57 -0300

    soporte para carts

--- controller/cart.controller.js
+++ controller/cart.controller.js
@@ -60,38 +60,78 @@ export const update = async (req, res) => {
 
 ///remove
 export const remove = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const cart = await service.remove(id);
-      if (!cart) {
-        res.status(404).json({ message: "could not remove cart" });
-      } else {
-        res.status(200).json(cart);
-      }
-    } catch (error) {
-      next(error.message);
+  try {
+    const { id } = req.params;
+    const cart = await service.remove(id);
+    if (!cart) {
+      res.status(404).json({ message: "could not remove cart" });
+    } else {
+      res.status(200).json(cart);
     }
-  };
-  
+  } catch (error) {
+    next(error.message);
+  }
+};
 
 /// add prod
 export const addProdToCart = async (req, res) => {
-    try {
-      const { idCart, idProd } = req.params;
-      const cart = await service.update(id, req.body);
-      if (!cart) {
-        res.status(404).json({ message: "could not update cart" });
-      } else {
-        res.status(200).json(cart);
-      }
-    } catch (error) {
-      next(error.message);
+  try {
+    const { idCart, idProd } = req.params;
+    const cart = await service.addProdToCart(idCart, idProd);
+    if (!cart) {
+      res.status(404).json({ message: "could not add product to cart" });
+    } else {
+      res.status(200).json(cart);
     }
-  };
-  
+  } catch (error) {
+    next(error.message);
+  }
+};
 
 /// remove prod
+export const removeProdFromCart = async (req, res) => {
+  try {
+    const { idCart, idProd } = req.params;
+    const cart = await service.removeProdFromCart(idCart, idProd);
+    if (!cart) {
+      res.status(404).json({ message: "could not remove product to cart" });
+    } else {
+      res.status(200).json(cart);
+    }
+  } catch (error) {
+    next(error.message);
+  }
+};
 
 /// update prod cant
+export const updProdQtyFromCart = async (req, res) => {
+  try {
+    const { idCart, idProd } = req.params;
+    const { quantity } = req.body;
+    const cart = await service.updProdQtyFromCart(idCart, idProd, quantity);
+    if (!cart) {
+      res
+        .status(404)
+        .json({ message: "could not change quantity of product in cart" });
+    } else {
+      res.status(200).json(cart);
+    }
+  } catch (error) {
+    next(error.message);
+  }
+};
 
-/// limpiar cart
+/// clean cart
+export const cleanCart = async (req, res) => {
+  try {
+    const { idCart } = req.params;
+    const cart = await service.cleanCart(idCart);
+    if (!cart) {
+      res.status(404).json({ message: "could not clean the cart" });
+    } else {
+      res.status(200).json(cart);
+    }
+  } catch (error) {
+    next(error.message);
+  }
+};
